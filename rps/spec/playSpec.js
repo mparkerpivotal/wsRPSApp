dea811f6def1a8e5a8c844628fc2e5f20dcc8e62
mparker <mparker@pivotal.io>
2018-03-08 13:51:59 -0500

    Refactoring Option #1: Extract Method /Composed Method refactoring—at the class level

--- rps/spec/playSpec.js
+++ rps/spec/playSpec.js
@@ -1,13 +1,25 @@
 function Rps(){
     this.play = function(p1, p2, observer){
-        if (!["rock", "paper", "scissors"].includes(p1) || !["rock", "paper", "scissors"].includes(p2))
+        if (invalid(p1) || invalid(p2))
             observer.invalid()
-        else if (p1 === p2)
+        else if (draw(p1, p2))
             observer.tie()
-        else if (p1 === "rock" && p2 === "scissors" || p1 === "scissors" && p2 === "paper" || p1 === "paper" && p2 === "rock")
+        else if (p1Wins(p1, p2))
             observer.p1Wins()
         else
             observer.p2Wins()
+    }
+
+    function invalid(p1) {
+        return !["rock", "paper", "scissors"].includes(p1);
+    }
+
+    function draw(p1, p2) {
+        return p1 === p2;
+    }
+
+    function p1Wins(p1, p2) {
+        return p1 === "rock" && p2 === "scissors" || p1 === "scissors" && p2 === "paper" || p1 === "paper" && p2 === "rock";
     }
 }
 
