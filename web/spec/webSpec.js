cf5a2397480434e96bcd87085add3f63defda730
mparker <mparker@pivotal.io>
2018-03-08 15:49:12 -0500

    first failing test on the frontend

--- web/spec/webSpec.js
+++ web/spec/webSpec.js
@@ -1,5 +1,33 @@
-describe("foo", function () {
-    it("bars", function () {
-        expect(true).toBe(false)
+const ReactDOM = require("react-dom")
+const React = require("react")
+
+class PlayForm extends React.Component {
+    render(){
+        return <button>Hello World!</button>
+    }
+}
+
+describe("play form", function () {
+
+    describe("request processes as invalid", function () {
+        it("display 'INVALID'", function () {
+            let domFixture = document.createElement("div")
+            domFixture.id = "hello!!!"
+            document.body.appendChild(domFixture)
+
+            let alwaysInvalidRps = {
+                play(p1, p2, observer){
+                    observer.invalid()
+                }
+            }
+
+            ReactDOM.render(
+                <PlayForm rps={alwaysInvalidRps}/>,
+                domFixture
+            )
+
+            document.querySelector("button").click()
+            expect(domFixture.innerText).toContain("INVALID")
+        })
     })
 })
