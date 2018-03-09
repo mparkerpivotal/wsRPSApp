63da743aa6beabb3a36277017961394693334971
Pivotal <pivotal@MacBook-Pro-6.local>
2018-03-09 10:40:10 -0500

    first two frontend tests  passing

--- web/spec/webSpec.js
+++ web/spec/webSpec.js
@@ -9,7 +9,15 @@ class PlayForm extends React.Component {
     }
     
     handleSubmit() {
+        this.props.rps.play("p1 throw placeholder", "p2 throw placeholder", this)
+    }
+
+    invalid(){
         this.setState({result: "INVALID"})
+    }
+
+    tie(){
+        this.setState({result: "TIE"})
     }
 
     render() {
@@ -30,6 +38,18 @@ describe("play form", function () {
             expect(page()).not.toContain("INVALID")
             submitForm()
             expect(page()).toContain("INVALID")
+        })
+    })
+
+    describe("request processes as tie", function () {
+        beforeEach(function () {
+            renderForm({play(p1, p2, observer){observer.tie()}})
+        })
+
+        it("display 'TIE'", function () {
+            expect(page()).not.toContain("TIE")
+            submitForm()
+            expect(page()).toContain("TIE")
         })
     })
 
